e5e8bb00c80dd1883919e7e85e07dc789246359a
Luna <125506519+LunaGrandjean@users.noreply.github.com>
2025-03-10 11:15:21 +0100

    Update server.js to start the server on port 5000

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server on a port from .env
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
