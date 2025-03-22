7cb7ee8f51cd724a05bdf6465952b91194bac29b
Mohammed ADDI <132593906+ai-mohammed@users.noreply.github.com>
2025-03-22 15:16:29 +0000

    fix small bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // this is the default, explicitly stating it helps clarity
+  },
+});
