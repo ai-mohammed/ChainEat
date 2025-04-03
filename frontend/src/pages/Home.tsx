0579ad2d206dfbd58ade22b47a6bfccb413d2362
Luna <125506519+LunaGrandjean@users.noreply.github.com>
2025-04-03 21:54:44 +0200

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,14 @@
+import "../App.css"; 
+
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>
-        Welcome to <span className="highlight">Chaineats</span>
-      </h1>
-      <p>Discover and book the best restaurants around you.</p>
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>
+          Welcome to <span className="highlight">ChainEats</span>
+        </h1>
+        <p>Discover and book the best restaurants around you.</p>
+      </div>
     </div>
   );
 };
