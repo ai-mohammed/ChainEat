8e716ba021ce997a3870fe468ef20e5553092561
Mohammed ADDI <132593906+ai-mohammed@users.noreply.github.com>
2025-04-17 10:15:48 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // this is the default, explicitly stating it helps clarity
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
